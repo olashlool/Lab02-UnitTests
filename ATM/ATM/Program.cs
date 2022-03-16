b514e700bc46a5dc07e12951e131b22bc2099af1
olashlool <olashlool94@gmail.com>
2022-03-16 21:18:17 +0200

    Unit Test

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace BankATM
+{
+    public class Program
+    {
+        static public decimal balance = 1000.500M;
+        static void Main(string[] args)
+        {
+            try {
+                UserInterface();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Oops, you have error {e.Message}");
+            }
+        }
+
+        public static void UserInterface()
+        {
+            Console.WriteLine("Welcome to Cairo Amman Bank. Thank you for choosing us for your financial needs.");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine();
+            bool transaction = true;
+            while (transaction)
+            {
+            Console.WriteLine("What would  you like to do? ");
+            Console.WriteLine("                          1. View Balance");
+            Console.WriteLine("                          2. Withdraw");
+            Console.WriteLine("                          3. Deposit");
+            Console.WriteLine("                          4. Exit");
+            Console.Write("Please select an option above: ");
+            int choose = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine();
+            //Console.WriteLine(choose);
+                switch (choose)
+                {
+                    case 1 :
+                        {
+                            Console.WriteLine($"Your current balance is: {ViewBalance()}$");
+                            Console.WriteLine();
+                            Console.WriteLine("-------------------------------------------------------------------------------");
+                            Console.Write("Do you want to make another transaction? (Y/N) ");
+                            string anotherchoose = Console.ReadLine();
+                            Console.WriteLine("-------------------------------------------------------------------------------");
+                            if (anotherchoose == "N" || anotherchoose == "n")
+                            {
+                                Console.Write("Thank you for using Cairo Amman Bank, see you next time ^-^ ");
+                                Console.WriteLine();
+                                transaction = false;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("How much would you like to withdraw ? ");
+                            decimal withdraw = Convert.ToDecimal(Console.ReadLine());
+                            Console.WriteLine($"Your NEW balance is: {Withdraw(withdraw)}$");
+                            Console.WriteLine();
+                            Console.WriteLine("-------------------------------------------------------------------------------");
+                            Console.Write("Do you want to make another transaction? (Y/N) ");
+                            string anotherchoose = Console.ReadLine();
+                            Console.WriteLine("-------------------------------------------------------------------------------");
+                            if (anotherchoose == "N" || anotherchoose == "n")
+                            {
+                                Console.Write("Thank you for using Cairo Amman Bank, see you next time ^-^ ");
+                                Console.WriteLine();
+                                transaction = false;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Write("How much would you like to Deposit ? ");
+                            decimal deposit = Convert.ToDecimal(Console.ReadLine());
+                            Console.WriteLine($"Your NEW balance is: {Deposit(deposit)}$");
+                            Console.WriteLine();
+                            Console.WriteLine("-------------------------------------------------------------------------------");
+                            Console.Write("Do you want to make another transaction? (Y/N) ");
+                            string anotherchoose = Console.ReadLine();
+                            Console.WriteLine("-------------------------------------------------------------------------------");
+                            if (anotherchoose == "N" || anotherchoose == "n")
+                            {
+                                Console.Write("Thank you for using Cairo Amman Bank, see you next time ^-^ ");
+                                Console.WriteLine();
+                                transaction = false;
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.Write("Thank you for using Cairo Amman Bank, see you next time ^-^ ");
+                            Console.WriteLine();
+                            transaction = false;
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("Not an option!");
+                        Console.WriteLine();
+                        Console.WriteLine("-------------------------------------------------------------------------------");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+
+            
+        }
+        public static decimal ViewBalance()
+        {
+            return balance;
+        }
+        public static decimal Withdraw(decimal withdraw)
+        {
+            if (withdraw < 0 || withdraw > balance)
+            {
+                while (withdraw < 0)
+                {
+                    Console.WriteLine("You cannot enter Withdraw less than 0 ");
+                    Console.Write("How much would you like to withdrawl? ");
+                    withdraw = Convert.ToDecimal(Console.ReadLine());
+                }
+                while (withdraw > balance)
+                {
+                    Console.WriteLine("You cannot enter Withdraw more than balance ");
+                    Console.Write("How much would you like to withdrawl? ");
+                    withdraw = Convert.ToDecimal(Console.ReadLine());
+                }
+            }
+            balance -= withdraw;
+            return balance;
+        }
+        public static decimal Deposit(decimal deposit)
+        {
+            if (deposit < 0 )
+            {
+                while (deposit < 0)
+                {
+                    Console.WriteLine("You cannot enter deposit less than 0 ");
+                    Console.Write("How much would you like to deposit? ");
+                    deposit = Convert.ToDecimal(Console.ReadLine());
+                }
+            }
+            balance += deposit;
+            return balance;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Lab02-UnitTests-
+# Lab02-UnitTests
+
+### Problem Domain:
+ The console displays options to either display the balance, withdraw money, deposit money, or exit the application.
+ There are a few exceptions: You cannot go to a negative balance; 
+ and you cannot input anything other than a number.
+
+#### How do you run the program?
+1. Clone repo to your device.
+2. Open the solution file ATM.sln in Visual Studio.
+3. To run the app, go to Debug > Start Without Debugging (or press ctrl+F5).
+4. The default starting balance is $1000.500.
+5. Select an option by entering a number between 1-4.
+6. If Withdrawing or Depositing, an amount will be requested.
+7. Enter a number, an error will be thrown if not a number.
+8. Continue selecting options until you are content.
+
+#### Home
+![Home](assest/home.png)
+
+#### Balance
+![Balance](assest/balance.png)
+
+#### Witgdraw
+![Witgdraw](assest/Witgdraw.png)
+
+
+#### Deposit
+![Deposit](assest/Deposit.png)
+
+
+#### Exit
+![Exit](assest/Exit.png)
+
+
+#### Error 
+##### [1]
+![Exit](assest/Error.png)
+
+##### [2]
+![Exit](assest/Error1.png)
+
+
